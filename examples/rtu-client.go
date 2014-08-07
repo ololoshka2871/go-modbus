0a7065f55b193b1066ca614336d38dd9ebfde00f
Denis Papathanasiou <dp2704@columbia.edu>
2014-08-07 13:53:54 -0400

    A more "real-world" example, whereby the slave device, start address, and number of bytes to read are provided by the command line args

--- examples/rtu-client.go
+++ examples/rtu-client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/dpapathanasiou/go-modbus"
 	"log"
 )
@@ -9,32 +10,38 @@ import (
 func main() {
 
 	// get the device serial port from the command line
-	var serialDevice string
-	const defaultInput = ""
-	flag.StringVar(&serialDevice, "serial", defaultInput, "Serial port (RS485) to use, e.g., /dev/ttyS0 (try \"dmesg | grep tty\" to find)")
-	// IRL: use more command-line arguments to define which registers to read or write...
+	var (
+		serialPort  string
+		slaveDevice int
+		startAddr   int
+		numBytes    int
+	)
+
+	const (
+		defaultPort         = ""
+		defaultSlave        = 1
+		defaultStartAddress = 3030
+		defaultNumBytes     = 16
+	)
+
+	flag.StringVar(&serialPort, "serial", defaultPort, "Serial port (RS485) to use, e.g., /dev/ttyS0 (try \"dmesg | grep tty\" to find)")
+	flag.IntVar(&slaveDevice, "slave", defaultSlave, fmt.Sprintf("Slave device number (default is %d)", defaultSlave))
+	flag.IntVar(&startAddr, "start", defaultStartAddress, fmt.Sprintf("Start address (default is %d)", defaultStartAddress))
+	flag.IntVar(&numBytes, "bytes", defaultNumBytes, fmt.Sprintf("Number of bytes to read from the start address (default is %d)", defaultNumBytes))
 	flag.Parse()
 
-	if len(serialDevice) > 0 {
+	if len(serialPort) > 0 {
 
 		// turn on the debug trace option, to see what is being transmitted
 		trace := true
 
-		// attempt to read the 200 address register on
-		// slave device number 1 (0x01) at serialDevice
-		readResult, readErr := modbusclient.RTURead(serialDevice, 0x01, modbusclient.FUNCTION_READ_HOLDING_REGISTERS, 200, 1, trace)
+		// attempt to read the [startAddr] address register on
+		// slave device number [slaveDevice] via the [serialDevice]
+		readResult, readErr := modbusclient.RTURead(serialPort, byte(slaveDevice), modbusclient.FUNCTION_READ_HOLDING_REGISTERS, uint16(startAddr), uint16(numBytes), trace)
 		if readErr != nil {
 			log.Println(readErr)
 		}
 		log.Println(readResult)
-
-		// attempt to write to a single coil at address 300
-		// on slave device number 1 (0x01) at serialDevice
-		writeResult, writeErr := modbusclient.RTUWrite(serialDevice, 0x01, modbusclient.FUNCTION_WRITE_SINGLE_COIL, 300, 1, []byte{0xff}, trace)
-		if writeErr != nil {
-			log.Println(writeErr)
-		}
-		log.Println(writeResult)
 
 	} else {
 
